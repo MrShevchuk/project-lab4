28e975577b08b6f05825fef636a37f50a2527f97
MrShevchuk <viacheslavshevchuk911@gmail.com>
2020-04-05 19:10:26 +0300

    done proj 4..

--- lab4/src/ua/zp/brainacad/Lab4Test.java
+++ lab4/src/ua/zp/brainacad/Lab4Test.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /**
  * This is prototype for Lab 4.
  */
-public class Lab4Test {
+public class Lab4Test<i> {
 
     public static void main(String[] args) {
 
@@ -14,12 +14,12 @@ public class Lab4Test {
         float[] firstArray;// TODO init it's array by new with size 4.
         firstArray = new float[4];
 
-        firstArray [0] = 5;
-        firstArray [3] = 13;
+        firstArray[0] = 5;
+        firstArray[3] = 13;
 
         // TODO insert some value to start of array and to end of array.
 
-        int[] intArray = {1,5,3,78,5,6}; // TODO init and fill it's array with any values. Use {} syntax.
+        int[] intArray = {1, 5, 3, 78, 5, 6}; // TODO init and fill it's array with any values. Use {} syntax.
 
         // 1.2)
         int[] intArrayCopy = Arrays.copyOf(intArray, 6); // TODO copy "intArray". Use copyOf...
@@ -32,9 +32,9 @@ public class Lab4Test {
         System.out.println(Arrays.toString(intArrayCopy));
         // 1.5)
         // TODO compare "intArray"  and "intArrayCopy", use Arrays equals. Print "Arrays equals" or "Arrays not equals".
-        Arrays.equals(intArray,intArrayCopy);
-        if (intArray==intArrayCopy) {
-        System.out.println("Arrays equals");
+        Arrays.equals(intArray, intArrayCopy);
+        if (intArray == intArrayCopy) {
+            System.out.println("Arrays equals");
         } else {
             System.out.println("Arrays not equals");
         }
@@ -43,26 +43,26 @@ public class Lab4Test {
         int[] testArray = {1, 3, 5, 7, 8, 6, 4, 2, 0};
 
         // Print array values in cycle.
-        for (int value: testArray) {
+        for (int value : testArray) {
             System.out.println(value + " ");
-       }
+        }
 
         // 2.1)
         // TODO calc sum of all array elements and print result.
         int[] testArraySum = {};
         testArraySum = Arrays.copyOf(testArray, testArray.length);
         int sumArr = 0;
-        for (int value: testArraySum) {
+        for (int value : testArraySum) {
             sumArr += value;
-            System.out.println (value + " = " + sumArr);
+            System.out.println(value + " = " + sumArr);
 
         }
 
         // 2.2)
         // TODO calc sum of all numbers with odd indexes and print result.
-        int [] testOddNum = Arrays.copyOf(testArray, testArray.length);
+        int[] testOddNum = Arrays.copyOf(testArray, testArray.length);
         int sumOdd = 0;
-        for (int value: testOddNum) {
+        for (int value : testOddNum) {
             if (value % 2 == 0) {
                 System.out.println();
             } else {
@@ -73,26 +73,60 @@ public class Lab4Test {
 
         // 2.3)
         // TODO find max value in array.
+
+        //1-способ
+
         int[] testArraySumMax = {};
         testArraySumMax = Arrays.copyOf(testArray, testArray.length);
         Arrays.sort(testArraySumMax);
-        int max = testArraySumMax[testArraySumMax.length-1];
+        int max = testArraySumMax[testArraySumMax.length - 1];
         System.out.println(max);
 
+        //2-й способ
 
-
+        int[] testArrayMaxV = Arrays.copyOf(testArray,9);
+        int maxIndex = 0;
+        for (int i = 0; i <testArrayMaxV.length ; i++) {
+            if(testArrayMaxV[i]>maxIndex)
+                maxIndex = testArrayMaxV[i];
+        }
+        System.out.println(maxIndex);
 
 
         // PART 3
 
         //3.1
         // TODO create two-dimensional array with size [3][4]
+        int[][] Arr2d = new int[3][4];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(Arr2d[i][j]);
+            }
+            System.out.println();
+        }
+
 
         //3.2
         // TODO fill array with any numbers in cycles.
 
+        for (int i = 0; i < Arr2d.length; i++) {
+            for (int j = 0; j < Arr2d[i].length; j++) {
+                Arr2d[i][j] = (i * 3) + (j + 1);
+                System.out.print(Arr2d[i][j]);
+            }
+            System.out.println();
+        }
+
+
         //3.3
         // TODO calc sum of all array elements and print result.
+        int sum = 0;
+        for (int i = 0; i < Arr2d.length; i++) {
+            for (int j = 0; j < Arr2d[i].length; j++) {
+                sum += Arr2d[i][j];
+            }
+        }
+        System.out.println("Сумма = " + sum);
 
 
         // PART 4*
@@ -100,12 +134,19 @@ public class Lab4Test {
         long[][] matrix = new long[9][9];
 
         int ratio = 0;
-        for (long[] row: matrix ) {
+        for (long[] row : matrix) {
             Arrays.fill(row, ++ratio);
         }
-
-        int sum = 0;
+        int sumN = 0;
         // TODO* calc sum only max and min values of matrix by expression: 2 < i <= 5 and only every third j value.
-        System.out.println(sum);
+        for (int i = 0; i > 2; i++) {
+            for (int j = 0; j <= 5; j++) {
+                if (j % 3 == 0) {
+                    System.out.println();
+                }
+                sumN += i;
+            }
+            System.out.println(sumN);
+        }
     }
 }
